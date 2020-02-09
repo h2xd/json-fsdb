fec188d08479c3a333608d6a4ddf7718ac7fccc9
Andreas Schujkow <mail@andreasschujkow.de>
2020-02-09 21:56:47 +0100

    refactored the name of the sync function

--- src/methods/addToCollection.ts
+++ src/methods/addToCollection.ts
@@ -1,14 +1,14 @@
 import { ArrayType, DatabaseBinding } from '../types';
 
 export function addToCollection<Schema, K extends keyof Schema>(methods: DatabaseBinding<Schema>) {
-  const { memory, key, sync } = methods;
+  const { memory, key, hibernate } = methods;
 
   /**
    * add entry to the collection
    * @param data data to be stored
    */
-  return function add(data: ArrayType<Schema[K]>) {
+  return async function add(data: ArrayType<Schema[K]>) {
     memory[key].push(data);
-    sync();
+    await hibernate();
   }
 }
